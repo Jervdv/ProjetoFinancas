f8ee3e943d5b2c07c5b90f4f9fc842c72c11fca3
unknown <jeremyvdvinne@gmail.com>
2023-05-30 17:33:27 -0300

    add gui

--- src/service/TransacaoService.java
+++ src/service/TransacaoService.java
@@ -1,15 +1,31 @@
 package service;
 
 import entities.Categoria;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author User
  */
 public class TransacaoService {
-    public Categoria cadastrarCategoria(String nome, int id){
-        return new Categoria(nome, id);
+    
+    private List<Categoria> categorias;
+
+    public TransacaoService() {
+        categorias = new ArrayList<Categoria>();
+        
     }
+
+    public List<Categoria> buscarCategorias(){
+        
+        return categorias;
+    }
+    
+    public void cadastrarCategoria(Categoria cat){
+        this.categorias.add(cat);
+    }
+    
     public void editarCategoria(){
         
     }
